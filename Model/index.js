e506c3322fbb2f2de3ab9fed69e2d707e25a2085
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2022-01-27 15:44:38 +0530

    added jobs models

--- Model/index.js
+++ Model/index.js
@@ -1,2 +1,3 @@
 export { default as User } from './users';
 export { default as Company } from './company';
+export { default as Job } from './jobs';
---
+++ Model/jobs.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const jobSchema = new Schema({
+    name: { type: String, required: true },
+    company_id: { type: String, required: true },
+    company_name: { type: String, required: true },
+    company_location: { type: String, required: true },
+    experience: { type: Number, required: true },
+    work_type: { type: String, required: true },
+    salary: { type: Number, required: true },
+    is_active: { type: Boolean, required: false, default: true }
+}, { timestamps: { currentTime: () => Math.floor(Date.now() / 1000) } })
+
+
+export default mongoose.model('Job', jobSchema, 'jobs');
