4b6cd691328c2b615d51902fe81ff10eb3b2807a
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2022-01-27 12:24:44 +0530

    user routes added

---
+++ Router/userRoutes.js
@@ -0,0 +1,18 @@
+import express from 'express';
+import { userController } from '../Controller'
+
+
+const router = express.Router();
+
+
+router.get('/', userController.getUsers);
+
+router.get('/:id', userController.getOneUser);
+
+router.post('/', userController.addUser);
+
+router.put('/:id', userController.updateUser);
+
+router.delete('/:id', userController.deleteUser);
+
+export default router;
