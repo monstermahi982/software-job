ad546b04b975dd91070474ce7bd4a1092665ea4e
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2022-01-27 15:09:59 +0530

    added company config and middleware config

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ import express from 'express';
 import mongoose from 'mongoose'
 import cors from 'cors'
 
-import { userRoutes } from './Router/';
+import { userRoutes, companyRoutes } from './Router/';
 import { DB_URL, PORT } from './Config'
+import { errorHandler } from './Middleware';
 
 const app = express();
 
@@ -21,6 +22,9 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use('/api/user', userRoutes)
+app.use('/api/company', companyRoutes)
+
+app.use(errorHandler)
 
 
 app.listen(PORT, () => console.log(`app listen on ${PORT} port`));
