a8e4d02e5f83a4b9c5afd5795a50e76287b20ead
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2022-01-28 11:35:25 +0530

    company name and location is removed

--- Model/jobs.js
+++ Model/jobs.js
@@ -4,8 +4,6 @@ const { Schema } = mongoose;
 const jobSchema = new Schema({
     name: { type: String, required: true },
     company_id: { type: String, required: true },
-    company_name: { type: String, required: true },
-    company_location: { type: String, required: true },
     experience: { type: Number, required: true },
     work_type: { type: String, required: true },
     salary: { type: Number, required: true },
