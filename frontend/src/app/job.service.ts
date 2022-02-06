0235557b5bca32972a883274847c8e768b2f1d81
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2022-02-06 23:57:44 +0530

    jobs services added

---
+++ frontend/src/app/job.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+import { JwtHelperService } from "@auth0/angular-jwt";
+import { CookieService } from 'ngx-cookie-service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JobService {
+  helper = new JwtHelperService();
+
+  constructor(private http:HttpClient, private cookie: CookieService) { }
+
+  getAllPosts(){
+    let url = "http://localhost:5000/api/jobs/search";
+    return this.http.get(url);
+  }
+
+  applyJob(jobData: any){
+    
+    const token = JSON.parse(this.cookie.get('token'));
+    const tokenData = this.helper.decodeToken(token); 
+    console.log(token, "---------as");
+    
+    const user_id = tokenData.id;
+    console.log(user_id);
+    
+    const data: any = {
+      user_id,
+      job_id: jobData._id,
+      company_id: jobData.company_id
+    }
+    console.log(data);
+    
+
+    let url = "http://localhost:5000/api/application";
+    return this.http.post(url, data);
+  }
+
+}
