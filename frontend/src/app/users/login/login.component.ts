240cd6934b7406d97dcbb35f3eab7e4e53e704de
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2022-02-06 23:41:37 +0530

    add router cookie removed reactive form

--- frontend/src/app/users/login/login.component.ts
+++ frontend/src/app/users/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
+import { UserService } from '../../user.service';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { JwtHelperService } from "@auth0/angular-jwt";
 
 @Component({
   selector: 'app-login',
@@ -7,21 +11,30 @@ import {FormControl, Validators} from '@angular/forms';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  helper = new JwtHelperService();
+  tokenData: any = {};
 
-  constructor() { }
+  constructor(private user:UserService, private router: Router, private cookie: CookieService) { }
 
   ngOnInit(): void {
-  }
 
-  email = new FormControl('', [Validators.required, Validators.email]);
-  password = new FormControl('', [Validators.required]);
-
-  getErrorMessage() {
-    if (this.email.hasError('required')) {
-      return 'You must enter a value';
+    if(this.cookie.get('token')){
+      this.router.navigate(['/', 'user']);
+      return; 
     }
 
-    return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  getLoginData(data: any){
+    console.log(data);
+    this.user.userLogin(data).subscribe((data) => {
+      console.log(data);
+      this.tokenData = this.helper.decodeToken(JSON.parse(JSON.stringify(data)));
+      this.cookie.set('name', this.tokenData.name);
+      this.cookie.set('token', JSON.stringify(data));
+      this.router.navigate(['/', 'user']);
+      
+    }) 
   }
 
 }
