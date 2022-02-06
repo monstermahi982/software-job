2ff1393841d2f5584c38b6026d1ce45236ceb5f4
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2022-02-06 23:56:42 +0530

    company services added

---
+++ frontend/src/app/company.service.ts
@@ -0,0 +1,56 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CompanyService {
+
+  constructor(private http:HttpClient) { }
+
+  getJobs(id: string){
+    let url = `http://localhost:5000/api/application/company/${id}`;
+    return this.http.get(url);
+  }
+
+  getCompanyDetail(id: any){
+    let url = `http://localhost:5000/api/company/${id}`;
+    return this.http.get(url);
+  }
+
+  companyLogin(data: any){
+    let url = "http://localhost:5000/api/company/login";
+    return this.http.post(url, data);
+  }
+
+  companyRegister(data: any){
+    let url = "http://localhost:5000/api/company";
+    return this.http.post(url, data);
+  }
+
+  addJob(data: any){
+    let url = "http://localhost:5000/api/jobs";
+    return this.http.post(url, data);
+  }
+
+  deleteJob(id: string){
+    let url = `http://localhost:5000/api/jobs/${id}`;
+    return this.http.delete(url);
+  }
+
+  blockJob(id: string){
+    let url = `http://localhost:5000/api/jobs/block/${id}`;
+    return this.http.put(url, {});
+  }
+
+  unblockJob(id: string){
+    let url = `http://localhost:5000/api/jobs/unblock/${id}`;
+    return this.http.put(url, {});
+  }
+
+  getCandidateByPost(id:string){
+    let url = `http://localhost:5000/api/application/job/${id}`;
+    return this.http.get(url);
+  }
+
+}
