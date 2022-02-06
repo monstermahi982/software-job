0dedb1796cc84150a109b2ec0dfd0faab75f600d
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2022-02-06 23:41:57 +0530

    add router cookie removed reactive form

--- frontend/src/app/users/register/register.component.ts
+++ frontend/src/app/users/register/register.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
+import { JwtHelperService } from "@auth0/angular-jwt";
+import { UserService } from '../../user.service';
 
 @Component({
   selector: 'app-register',
@@ -8,9 +12,18 @@ import {FormControl, Validators} from '@angular/forms';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  helper = new JwtHelperService();
+  tokenData: any = {};
+
+  constructor(private user:UserService, private router: Router, private cookie: CookieService) { }
 
   ngOnInit(): void {
+
+    if(this.cookie.get('token')){
+      this.router.navigate(['/', 'user']);
+      return; 
+    }
+
   }
 
   email = new FormControl('', [Validators.required, Validators.email]);
@@ -24,6 +37,18 @@ export class RegisterComponent implements OnInit {
     }
 
     return this.email.hasError('email') ? 'Not a valid email' : '';
+  }
+
+  getRegData(data: any){
+    console.log(data);
+    this.user.userReg(data).subscribe((data) => {
+      console.log(data);
+      this.tokenData = this.helper.decodeToken(JSON.parse(JSON.stringify(data)));
+      this.cookie.set('name', this.tokenData.name);
+      this.cookie.set('token', JSON.stringify(data));
+      this.router.navigate(['/', 'user']);
+      
+    })
   }
 
 }
