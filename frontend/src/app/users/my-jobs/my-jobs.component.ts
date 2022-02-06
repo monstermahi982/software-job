2d025656085d18b6fe55ddcc87d184c9caa88be2
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2022-02-06 23:42:53 +0530

    add backend

--- frontend/src/app/users/my-jobs/my-jobs.component.ts
+++ frontend/src/app/users/my-jobs/my-jobs.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../../user.service';
+import { JwtHelperService } from "@auth0/angular-jwt";
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-my-jobs',
@@ -7,9 +11,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyJobsComponent implements OnInit {
 
-  constructor() { }
+  data: any = [];
+  helper = new JwtHelperService();
+
+  constructor(private user: UserService, private cookie: CookieService, private router: Router) { 
+    
+  }
+
+  token = "";
+  tokenData: any = {};
+
 
   ngOnInit(): void {
+
+    if(!this.cookie.get('token')){
+      this.router.navigate(['/', 'user', 'login']);
+      return; 
+    }
+    
+    this.getUserApplication();
+  }
+  
+  getUserApplication(){
+    
+    this.token = JSON.parse(this.cookie.get('token'));
+    // console.log(this.token);
+  
+    this.tokenData =  this.helper.decodeToken(this.token);
+    // console.log(this.tokenData.id);
+    
+    this.user.getData(this.tokenData.id).subscribe(data => {
+      // console.log(data);
+      this.data = data;
+      // console.log(this.data);
+      
+    })
+
+  }
+
+
+  deleteApplication(id: string){
+    console.log(id);
+    this.user.deleteApplication(id).subscribe((data) => {
+      console.log(data);
+      this.getUserApplication();
+    })
+    
   }
 
 }
