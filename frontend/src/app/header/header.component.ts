4a383c09cf51c4744457c72dbdbba79a8fd8c85c
Mahesh Gaikwad <maheshgaikwad8892@gmail.com>
2022-02-06 23:38:50 +0530

    added login logout logic

--- frontend/src/app/header/header.component.ts
+++ frontend/src/app/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  userName: string = "";
+  is_logged: boolean = false;
+  token: string = "";
+
+  constructor(private cookie: CookieService, private router:Router) { }
 
   ngOnInit(): void {
+    // console.log( "---------------------");
+    
+    // this.token = this.cookie.get('token');
+    // if(this.token !== ""){
+    //   console.log("token is there");
+      
+    // }else{
+    //   console.log("no");
+      
+    // }
+
+  }
+
+  loggedin(){
+    this.userName = this.cookie.get('name');
+    return this.cookie.get('token');
+  }
+
+  logout(){
+    console.log("-----------");
+    
+    this.cookie.deleteAll();
+    this.router.navigate(['/']);
   }
 
 }
